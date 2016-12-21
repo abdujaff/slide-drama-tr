c00f6a0e3caf4c2d1e55d7385d03261f26da15fd
abdujaff <abdujaff@gmail.com>
2016-12-21 23:53:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,4 @@ jQuery.extend(jQuery.easing,{easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){r
 // Obfuscated by Javascript Obfuscator
 // http://javascript-source.com
 //***********************************************
-jQuery("#wowslider-container1").wowSlider({effect:"carousel_basic",prev:"",next:"",duration:20*100,delay:20*100,width:830,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
+jQuery("#wowslider-container1").wowSlider({effect:"carousel_basic",prev:"",next:"",duration:22*100,delay:52*100,width:830,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
